1eb8a3a5de9a154f5b9daf3c34b60384e343261e
Kowshik Sundararajan <ksundararajan@authentic8.com>
2017-10-15 02:59:28 +0800

    fix for deploy

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,15 +1,14 @@
 var mongoose = require('mongoose');
-var env = require('../env.json');
+// var env = require('../env.json');
 
 // Making mongoose use the default promise and not a third-party promise
 mongoose.Promise = global.Promise;
 
-var DB_USERNAME = process.env.DB_USERNAME || env['DB_USERNAME'];
-var DB_PASSWORD = process.env.DB_PASSWORD || env['DB_PASSWORD'];
+// var DB_USERNAME = process.env.DB_USERNAME || env['DB_USERNAME'];
+// var DB_PASSWORD = process.env.DB_PASSWORD || env['DB_PASSWORD'];
 
-//console.log(DB_USERNAME);
 
-var uri = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@ds119675.mlab.com:19675/intuition`;
+var uri = `mongodb://kowshik:kowshik@ds119675.mlab.com:19675/intuition`;
 global.db = mongoose.connect(uri, {
     useMongoClient: true
 });
