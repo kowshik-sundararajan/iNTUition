e4235e36ce7ef2c5b2125703e41dd034ae4c8ed0
Kowshik Sundararajan <ksundararajan@authentic8.com>
2017-10-15 02:44:42 +0800

    Setup mLab

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,9 +1,22 @@
 var mongoose = require('mongoose');
+var env = require('../env.json');
 
 // Making mongoose use the default promise and not a third-party promise
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/iNTUition', {
+
+var DB_USERNAME = process.env.DB_USERNAME || env['DB_USERNAME'];
+var DB_PASSWORD = process.env.DB_PASSWORD || env['DB_PASSWORD'];
+
+//console.log(DB_USERNAME);
+
+var uri = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@ds119675.mlab.com:19675/intuition`;
+global.db = mongoose.connect(uri, {
     useMongoClient: true
 });
+
+global.db.on('error', console.error.bind(console, 'connection error:'));
+// mongoose.connect('mongodb://kowshik:kowshik@ds119675.mlab.com:19675/intuition', {
+//     useMongoClient: true
+// });
 
 module.exports = {mongoose};
