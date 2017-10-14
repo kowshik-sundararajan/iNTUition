fa08625b3388cc2674a42ead2bb9044dcb2a0777
Kowshik Sundararajan <ksundararajan@authentic8.com>
2017-10-15 04:39:17 +0800

    Html mockup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 var bodyParser = require('body-parser');
+const axios = require('axios');
 
 var {mongoose} = require('./db/mongoose');
 // var {User} = require('./models/User');
@@ -22,8 +23,24 @@ app.use('/users', require('./routes/user-routes'));
 app.use('/causes', require('./routes/cause-routes'));
 app.use('/organizations', require('./routes/organization-routes'));
 
+// var makeRequest = () => {
+//     return new Promise((resolve, reject) => {
+//         axios.get('http://localhost:3000/causes/').then((results) => {
+//             console.log(results['data']);
+//             resolve(results['data']);
+//         }).catch((error) => {
+//             reject(error);
+//         });     
+//     });
+// }
+
 app.use('/', (request, response) => {
-    response.render('test.hbs');
+    // makeRequest().then((data) => {
+    //     response.render('index.hbs', {
+    //         causes: data
+    //     });
+    // });
+    response.render('index.hbs');
 });
 
 app.listen(PORT, () => {
